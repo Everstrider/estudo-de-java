d7c8291c8dd011ff9998f77119640a9a0e63d508
Lucas Garcia <lucas8bernadochi@gmail.com>
2024-05-29 16:30:14 -0300

    Add files via upload
    Escreva um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel
    4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até
    que seja válido). O programa será encerrado quando o código informado for o número 4. Deve ser escrito a
    mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível,

---
+++ exercicio3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+        System.out.printf("1 - Álcool%n2 - Gasolina%n3 - Diesel%n4 - Fim%n");
+        System.out.printf("Digite o combustível desejado para abastecer o veículo:%n");
+        int tipo = sc.nextInt();
+
+        while (tipo != 4) {
+            switch (tipo) {
+                case 1:
+                    alcool += 1;
+                    System.out.printf("Álcool: %d%n", alcool);
+                    tipo = sc.nextInt();
+                    break;
+                case 2:
+                    gasolina += 1;
+                    System.out.printf("Gasolina: %d%n", gasolina);
+                    tipo = sc.nextInt();
+                    break;
+                case 3:
+                    diesel += 1;
+                    System.out.printf("Diesel: %d%n", diesel);
+                    tipo = sc.nextInt();
+                    break;
+                default:
+                    System.out.printf("Código inválido, digite outro:%n");
+                    tipo = sc.nextInt();
+            }
+        }
+        System.out.printf("Muito Obrigado!%n Alcool: %d%n Gasolina: %d%n Diesel %d%n", alcool, gasolina, diesel);
+
+        sc.close();
+    }
+}
